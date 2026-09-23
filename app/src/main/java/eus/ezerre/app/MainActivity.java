a53a86c8d7c7b17903a8804a4bc1411bd528fffc
Joanes Plazaola <joanes@ni.eus>
2026-09-23 08:00:41 +0000

    Gauzitak jeje

---
+++ app/src/main/java/eus/ezerre/app/MainActivity.java
@@ -0,0 +1,35 @@
+package eus.ezerre.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setDomStorageEnabled(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setBackgroundColor(0xFFF5F5F2);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
