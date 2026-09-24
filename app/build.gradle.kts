71c171f78788427bf4384c28fd28b2eda1ee9957
gedar_dev <teknikoa@gedar.eus>
2026-09-24 11:10:25 +0200

    Prepare v3.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "eus.ezerre.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "3.0.0"
     }
 }
